6ebe0a30f6dcb191b7df9b2f46235aec5e1a1113
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2025-01-18 19:35:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,32 +4,55 @@ const sortInputArray = (event) => {
   event.preventDefault();
 
   const inputValues = [
-    ...document.getElementsByClassName("values-dropdown"),
+    ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
 
-  const sortedValues = bubbleSort(inputValues);
+  const sortedValues = selectionSort(inputValues);
+
   updateUI(sortedValues);
-};
+}
 
 const updateUI = (array = []) => {
   array.forEach((num, i) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
     outputValueNode.innerText = num;
-  });
-};
+  })
+}
 
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
-        console.log(array,array[j],array[j+1])
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
     }
   }
-  if(array[j] > array[j+1]){
-        const temp = array[j];
-        array[j] = array[j +1];
-        array[j+1] = temp;
-  }
+
   return array;
-};
+}
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+
+  return array;
+}
+
+const insertionSort = (array)=>{
+    
+}
 
 sortButton.addEventListener("click", sortInputArray);
